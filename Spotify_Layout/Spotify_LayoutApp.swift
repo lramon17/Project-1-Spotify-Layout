db9487d96de2532650b150ed0f3803eb3d2729c0
Lillian Ramon <lramon@unc.edu>
2026-02-22 22:22:25 -0500

    Initial Commit

---
+++ Spotify_Layout/Spotify_LayoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  Spotify_LayoutApp.swift
+//  Spotify_Layout
+//
+//  Created by Lily Ramon on 2/22/26.
+//
+
+import SwiftUI
+
+@main
+struct Spotify_LayoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
